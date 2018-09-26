332ac3d7b7400d12b3fe8ffcc6ad245fa94a2d44
toversus <toversus2357@gmail.com>
2018-09-27 07:51:08 +0900

    [Day 29] p.203

---
+++ ch07/ex7-14/main_test.go
@@ -0,0 +1,70 @@
+package eval
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestEval(t *testing.T) {
+	tests := []struct {
+		expr string
+		env  Env
+		want string
+	}{
+		{"sqrt(A / pi)", Env{"A": 87616, "pi": math.Pi}, "167"},
+		{"pow(x, 3) + pow(y, 3)", Env{"x": 12, "y": 1}, "1729"},
+		{"pow(x, 3) + pow(y, 3)", Env{"x": 9, "y": 10}, "1729"},
+		{"5 / 9 * (F - 32)", Env{"F": -40}, "-40"},
+		{"5 / 9 * (F - 32)", Env{"F": 32}, "0"},
+		{"5 / 9 * (F - 32)", Env{"F": 212}, "100"},
+	}
+	var prevExpr string
+	for _, test := range tests {
+		// Print expr only when it changes.
+		if test.expr != prevExpr {
+			prevExpr = test.expr
+		}
+		expr, err := Parse(test.expr)
+		if err != nil {
+			t.Error(err) // parse error
+			continue
+		}
+		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
+		if got != test.want {
+			t.Errorf("%s: %v => %s, want %s", test.expr, test.env, got, test.want)
+		}
+	}
+}
+
+func TestString(t *testing.T) {
+	tests := []string{
+		"sqrt(A / pi)",
+		"pow(x, 3) + pow(y, 3)",
+		"5 / 9 * (F - 32)",
+	}
+
+	for _, test := range tests {
+		expr1, err := Parse(test)
+		if err != nil {
+			t.Error(err)
+		}
+		expr2, err := Parse(expr1.String())
+		if err != nil {
+			t.Error(err)
+		}
+		if expr1.String() != expr2.String() {
+			t.Errorf("%s != %s", expr1.String(), expr2.String())
+		}
+	}
+}
+
+func TestMinEval(t *testing.T) {
+	expr := &min{Var("x"), literal(15.0)}
+	env := Env{"x": 15.01}
+	want := 15.0
+
+	if got := expr.Eval(env); math.Abs(want-got) > 1e-7 {
+		t.Errorf("%+v.Eval(%+v)=%+v; want=%+v", expr, env, got, want)
+	}
+}
